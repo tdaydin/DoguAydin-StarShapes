d0abe03d3448be740699ad4f53254b6a666513db
Tayyip Doğu AYDIN <tdaydin@gmail.com>
2020-10-26 22:11:48 +0300

    v2 - minor bug fixes

--- StarShapes/Program.cs
+++ StarShapes/Program.cs
@@ -12,16 +12,36 @@ namespace StarShapes
         {
 
             string keepGoing = "Yes";
+            int input = 0;
+            string entry;
+            Console.WriteLine("This program creates triangles from Stars");
 
             while (keepGoing == "Yes" || keepGoing == "yes")
             {
-                
-                Console.WriteLine("For Right Angle Triangle, enter 1. For a Regular Triangle, enter 2");
-                int input = Convert.ToInt32(Console.ReadLine());
+                int check = 1;
 
+                while (check == 1)
+                {
+                    Console.WriteLine("For Right Angle Triangle, enter 1. For a Regular Triangle, enter 2");
 
+                    entry = Console.ReadLine();
+
+                    if (entry == "1" || entry == "2")
+                    {
+                        check = 0;
+                        input = Convert.ToInt32(entry);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You chose an invalid entry, please choose again");
+                        check = 1;
+                    }
+                }
+
+               
                 Console.WriteLine("How many lines do you want?");
                 int number = Convert.ToInt32(Console.ReadLine());
+
 
                 if (input == 1)
                 {
